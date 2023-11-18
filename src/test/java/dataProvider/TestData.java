643d80f5302a658e63ab510dbfe9d332de68ef4d
Aziza Sultana <azizaantora@gmail.com>
2023-11-19 04:07:12 +0600

    initial commit

---
+++ src/test/java/dataProvider/TestData.java
@@ -0,0 +1,32 @@
+package dataProvider;
+
+public class TestData {
+	// Basic Details
+	public String first_name = "Test";
+	public String last_name = "User";
+	public String name = "Test User";
+	public String username = "Test User";
+	public String password = "abc123";
+	public String days = "3";
+	public String months = "11";
+	public String years = "1990";
+	
+	
+	// Address
+	public String company = "Google";
+	public String address1 = "Toronto";
+	public String address2 = "Road 11";
+	public String country = "Canada";
+	public String state = "Toronto";
+	public String city = "New City";
+	public String zipcode = "1200";
+	public String mobile_number = "02135489";
+	public String message = "Test Comment";
+	
+	// Payment Information
+	public String name_on_card = "Test User";
+	public String card_number = "0000000000000000";
+	public String cvc = "1234";
+	public String expiry_month = "01";
+	public String expiry_year = "2026";
+}
---
+++ src/test/java/ui/HomePageTest.java
@@ -0,0 +1,11 @@
+package ui;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void homePageTest () {
+		System.out.println("Homepage Found");
+	}
+}
